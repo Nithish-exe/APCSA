bffa930dd098f3c206b4ef87c04c536797f87d80
Nithish Muthiah <emailnithish@gmail.com>
2024-11-19 11:33:32 -0500

    cow 9

---
+++ src/COW_9/BinaryFun.java
@@ -0,0 +1,49 @@
+package COW_9;
+
+import java.awt.*;
+import java.util.*;
+public class BinaryFun
+{
+    public int convertBinaryToDecimal(boolean [] binaryValues){
+        int num = 0;
+        return num;
+    }
+
+    public int convertHexadecimalToDecimal(char [] hexValues){
+	int num = 0;
+        return num;
+    }
+
+    public boolean[] addBinary(boolean [] binaryOne, boolean [] binaryTwo){
+        boolean [] binarySum = new boolean[0];
+        
+        return binarySum;
+    }
+
+    public char[] addHexadecimal(char [] hexOne, char [] hexTwo){
+        char [] hexSum = new char[0];
+        
+        return hexSum;
+
+    }
+
+    public char[] convertBinaryToHex(boolean [] binary){
+        char [] result = new char[0];
+        
+        return result;
+    }
+
+    public boolean [] convertDecimalToBinary(int decNumber){
+	   boolean [] result = new boolean[0];
+        
+        return result;
+    }
+
+    public char [] convertDecimalToHexadecimal(int decNumber){
+        char [] result = new char[0];
+        
+        return result;
+
+    }
+
+}
