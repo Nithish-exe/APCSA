536a9de7e80bb6f5d9e9d1176ce2d945cd04cc2e
Nithish Muthiah <emailnithish@gmail.com>
2024-12-18 11:26:48 -0500

    cow 10+11

---
+++ src/COW_10/Myrtle.java
@@ -0,0 +1,60 @@
+package COW_10;
+
+import java.util.*;
+import java.awt.*;
+import javax.swing.*;
+public class Myrtle extends Turtle
+{
+    public Myrtle(JPanel world, Block [][] map, char direction, int row, int col, int appleCount, int orangeCount, int lemonCount){
+        super(world, map, direction, row, col, appleCount, orangeCount, lemonCount);
+    }
+
+    public void pickRowOfApples(int numSteps){
+        
+    }
+
+    public void pickRowOfFruit(int numSteps){
+        
+    }
+
+    public void pickApplesToWall(int numApples){
+        
+    }
+
+    public void pickFruitToWall(int numApples, int numOranges, int numLemons){
+        
+    }
+
+    public void harvestRowAndDeposit(int length){
+        
+    }
+
+    public void placeRowOfFruit(int length, int numApples, int numOranges, int numLemons){
+        
+    }
+
+    public void harvestAllAndDeposit(int width, int height){
+        
+    }
+
+    public void placeGrid(int width, int height, int numApples, int numOranges, int numLemons){
+        
+    }
+
+    public void searchForFruit(){
+        
+    }
+
+    public void placeSpiral(int numSides){
+        
+    }
+
+    public void placePyramid(int sideLength){
+        
+    }
+
+    public void fillMazeWithFruits(){
+        
+    }
+
+}
---
+++ src/COW_10/Painter.java
@@ -0,0 +1,58 @@
+package COW_10;
+
+import java.awt.*;
+import java.util.*;
+
+public class Painter
+{
+    public void paintBoxGrid(Graphics g){
+        
+    }
+    
+    public void paintMultiplicationTable(Graphics g){
+        
+    }
+    
+    public void paintBrickWall(Graphics g){
+        
+    }
+    
+    public void paintZigZag(Graphics g){
+              
+    }
+    
+    public void paintWallPaper70s(Graphics g){
+        //How to generate a random Color:
+        //g.setColor(new Color((int)(255 * Math.random()), (int)(255 * Math.random()), (int)(255 * Math.random())));
+        
+    }
+
+    public void paintGridAndLines(Graphics g){
+        
+    }
+    
+    public void paintTubularTwo(Graphics g){
+        
+    }
+
+    public void paintPatchworkMax(Graphics g){
+        
+    }
+    
+    public void paintTiledFloor(Graphics g){
+        
+    }
+    
+    public void paintPatchWork(Graphics g){
+        
+    }
+    
+    public void paintRainbowFade(Graphics g){
+        
+    }
+    
+    public void paintDiscoDown(Graphics g){
+        
+    }
+    
+}
